b11b52a034037d16c69928168d20e433cf6dde94
ragulraj <rraj9350@gmail.com>
2025-03-18 19:16:52 +0530

    api intergration with shimmer effect

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,44 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import Header from './components/header/Header'
 import CardWrapper from './components/cardWrapper/CardWrapper'
-import { resDetails } from './utils/data'
+// import { resDetails } from './utils/data'
 import SearchWrapper from './components/searchWrapper/SearchWrapper'
+import axios from 'axios'
 
 const App = () => {
 
-  const[resList, setResList] = useState(resDetails);
   const[search, setSearch] = useState('');
+  const[resDetails, setResDetails] = useState([]);
+  const[resDetailsStatic, setResDetailsStatic] = useState([]);
+
+  useEffect( () => {
+
+    const fetchData = async () => {
+
+      const response = await axios.get("https://www.swiggy.com/dapi/restaurants/list/v5?lat=10.7904833&lng=78.7046725&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING");      
+      const restaurantsData = await response?.data?.data?.cards[1]?.card?.card?.gridElements?.infoWithStyle?.restaurants;
+      setResDetailsStatic(restaurantsData);
+      setResDetails(restaurantsData);
+
+    }
+
+    fetchData();
+
+  },[])
 
   const searchHandler = (e) => {
 
     e.preventDefault();  
-    search ? setResList(resList.filter( item => item.name.toLowerCase().includes(search.toLowerCase()))) : setResList(resDetails);
+    search ? setResDetails(resDetails.filter( item => item?.info?.name.toLowerCase().includes(search.toLowerCase()))) : setResDetails(resDetailsStatic);
 
-  }
+  }  
 
   return (
     <>
     <Header />
     <main>
       <SearchWrapper setSearch={setSearch} searchHandler={searchHandler}/>
-      <CardWrapper resDetails={resList}/>
+      <CardWrapper resDetails={resDetails}/>
     </main>
     </>
   )
